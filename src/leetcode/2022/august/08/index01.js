d8557f256c1a47754cb9bba539b5bf7969f750ef
duxinwu <13935741214@163.com>
2022-08-09 17:43:25 +0800

    feat: 搜索插入位置

---
+++ src/leetcode/2022/august/08/index01.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0
+  let right = nums.length - 1
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2)
+
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] > target) {
+      right = mid - 1
+    } else if (nums[mid] < target) {
+      left = mid + 1
+    }
+  }
+
+  return nums[left] < target ? left + 1 : left
+}
+
+let nums = [1, 3, 5, 6],
+  target = 7
+console.log(searchInsert(nums, target))
+console.log('👻👻👻👻👻👻👻👻👻👻')
