05c3bb72fe8311e15d6ca8df6e99c5d84c9e107a
duxinwu <13935741214@163.com>
2022-11-10 18:46:57 +0800

    feat: 实现bind

---
+++ src/javascript专题/手动实现call、apply、bind/实现bind.js
@@ -0,0 +1,16 @@
+/**
+ * 1、处理参数，返回一个闭包
+ * 2、判断是否为构造函数调用，如果是则使用 new 调用当前函数
+ * 3、如果不是，使用apply，将context和处理好的参数传入
+ */
+
+Function.prototype.myBind = function (context, ...args1) {
+  if (this === Function.prototype) {
+    return undefined
+  }
+  const that = this
+  return function F(...args2) {
+    // 判断是否用于构造函数
+    return that.apply(this instanceof F ? this : context, args1.concat(args2))
+  }
+}
