f713875140a9ed7ee97b072db354389c74db3aa4
duxinwu <13935741214@163.com>
2022-11-10 18:46:46 +0800

    feat: 实现apply

--- src/javascript专题/手动实现call、apply、bind/实现apply.js
+++ src/javascript专题/手动实现call、apply、bind/实现apply.js
@@ -2,14 +2,16 @@
  * apply 实现类似于 call，参数为数组
  */
 
-Function.prototype.myCall = function (context = globalThis, args) {
+Function.prototype.myApply = function (context = globalThis, args) {
   if (this === Function.prototype) {
-    return undefined // Function.prototype.myCall() 直接调用
+    return undefined // Function.prototype.myApply() 直接调用
   }
   context = context || globalThis
   const symbol = Symbol()
   context[symbol] = this
-  const result = context[symbol](...args)
+  const result = Array.isArray(args)
+    ? context[symbol](...args)
+    : context[symbol]()
   delete context[symbol]
   return result
 }
